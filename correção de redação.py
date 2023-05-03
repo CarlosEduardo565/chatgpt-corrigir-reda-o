ee686cbbdb6840f3fdcf83cd98dc06d5f8638350
Carlos Eduardo <carlos_eduardo565@hotmail.com>
2023-05-03 20:57:39 -0300

    atualização no pre prompt

--- correção de redação.py
+++ correção de redação.py
@@ -7,19 +7,19 @@ def armazenando_texto():
    root.quit()
 
 root = tk.Tk()
-root.geometry('300x100')
+root.geometry('600x600')
 caixa_texto = tk.Entry(root, width = 30)
 caixa_texto.pack(fill = tk.X, padx = 10, pady=10)
 
-botao = tk.Button(root, text = "Enviar mensagem", command = armazenando_texto)
+botao = tk.Button(root, text = "Nota da sua redação", command = armazenando_texto)
 botao.pack()
 root.mainloop()
 
 
 root.update()
 if texto:
-    openai.api_key = "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"
-    pre_prompt = "Você é um avaliador de redação oficial do ENEM. O tema da redação é feminismo Dê uma nota de 0 a 1000 para essa redação abaixo, seja rigoroso. Considerando os seguintes criterios: 1. Domínio da escrita formal em língua portuguesa, 2. Compreensão do tema e aplicação das áreas de conhecimento, 3. Capacidade de interpretação das informações e organização dos argumentos, 4. Domínio dos mecanismos linguísticos de argumentação, 5. Capacidade de conclusão com propostas coerentes que respeitem os direitos humanos.Leve em consideração que o tema da redação é feminismo. E depois disso cite os pontos positivos e negativos e o que tem a melhorar"
+    openai.api_key = "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"
+    pre_prompt = "Você é um avaliador de redação oficial do ENEM cujo tema é Feminicidio no Brasil, se a redação não tiver nada haver com o tema ela devera ser desconsiderada. Dê uma nota de 0 a 1000 para a redação seguindo as competências cobradas na redação do enem, seja rigoroso. Considerando os seguintes criterios: 1. Domínio da escrita formal da língua portuguesa, 2. Compreender o tema e não fugir do que é proposto, 3. Selecionar, relacionar, organizar e interpretar informações, fatos, opiniões e argumentos em defesa de um ponto de vista, 4. Conhecimento dos mecanismos linguísticos necessários para a construção da argumentação, 5. Respeito aos direitos humanos. Leve em consideração que o tema da redação é feminicidio. E depois disso cite os pontos positivos e negativos e o que tem a melhorar."
     solicitacao = texto
     pos_prompt = "\nA nota para essa redação é:"
     response = openai.Completion.create(
@@ -32,4 +32,4 @@ if texto:
       presence_penalty=0
     )
 print(response.choices[0].text)
-texto = ''
+texto = ''   
